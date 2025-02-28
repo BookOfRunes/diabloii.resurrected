3f0717bc24e1f75389a6e566f2618531c14aa6ea
Mihucza Bence (uids6410) <Bence.Mihucza-EXT@continental-corporation.com>
2025-02-28 16:08:56 +0100

    test: Fix failed tests

--- src/RuneGlossary.Resurrected.Test.Unit/WASM/Fakers/ItemTypeFaker.cs
+++ src/RuneGlossary.Resurrected.Test.Unit/WASM/Fakers/ItemTypeFaker.cs
@@ -3,18 +3,18 @@ using RuneGlossary.Resurrected.WASM.Models;
 
 namespace RuneGlossary.Resurrected.Test.Unit.WASM.Fakers
 {
-	public class ItemTypeFaker : AutoFaker<ItemType>
-	{
-		public ItemTypeFaker()
-		{
-			RuleFor(it => it.Selected, false);
-		}
+    public class ItemTypeFaker : AutoFaker<ItemType>
+    {
+        public ItemTypeFaker()
+        {
+            RuleFor(it => it.Selected, false);
+        }
 
-		public ItemTypeFaker Selected()
-		{
-			RuleFor(it => it.Selected, true);
+        public ItemTypeFaker Selected()
+        {
+            RuleFor(it => it.Selected, true);
 
-			return this;
-		}
-	}
+            return this;
+        }
+    }
 }
--- src/RuneGlossary.Resurrected.WASM/Models/ItemType.cs
+++ src/RuneGlossary.Resurrected.WASM/Models/ItemType.cs
@@ -2,19 +2,18 @@
 
 namespace RuneGlossary.Resurrected.WASM.Models
 {
-	public record ItemType : INotifyPropertyChanged
-	{
-		private bool selected;
+    public record ItemType : INotifyPropertyChanged
+    {
+        private bool _selected;
+        public bool Selected
+        {
+            get => _selected;
+            set { _selected = value; PropertyChanged?.Invoke(this, EventArgs.Empty); }
+        }
+        public required int Id { get; init; }
+        public required ItemClass Class { get; init; }
+        public required string Name { get; init; }
 
-		public bool Selected
-		{
-			get { return selected; }
-			set { selected = value; PropertyChanged?.Invoke(this, EventArgs.Empty); }
-		}
-		public required int Id { get; init; }
-		public required ItemClass Class { get; init; }
-		public required string Name { get; init; }
-
-		public event EventHandler<EventArgs> PropertyChanged;
-	}
+        public event EventHandler<EventArgs>? PropertyChanged;
+    }
 }
