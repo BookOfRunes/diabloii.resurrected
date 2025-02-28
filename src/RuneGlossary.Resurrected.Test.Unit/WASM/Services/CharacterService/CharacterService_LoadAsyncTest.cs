f937ccd4d035e75aca6b37b3eed262f02daac112
Bence Mihucza <mihben@gmail.com>
2025-02-28 16:14:41 +0100

    test: Fix failed tests (#9)
    Co-authored-by: Mihucza Bence (uids6410) <Bence.Mihucza-EXT@continental-corporation.com>

--- src/RuneGlossary.Resurrected.Test.Unit/WASM/Services/CharacterService/CharacterService_LoadAsyncTest.cs
+++ src/RuneGlossary.Resurrected.Test.Unit/WASM/Services/CharacterService/CharacterService_LoadAsyncTest.cs
@@ -9,68 +9,67 @@ using Xunit;
 
 namespace RuneGlossary.Resurrected.Test.Unit.WASM.Services
 {
-	public partial class CharacterServiceTest
-	{
-		[Fact(DisplayName = "[UNIT][CSV/LOAD-001] - Query Characters")]
-		public async Task CharacterService_LoadAsync_QueryCharacter()
-		{
-			// Arrange
-			var sut = CreateSUT();
+    public partial class CharacterServiceTest
+    {
+        [Fact(DisplayName = "[UNIT][CSV/LOAD-001] - Query Characters")]
+        public async Task CharacterService_LoadAsync_QueryCharacter()
+        {
+            // Arrange
+            var sut = CreateSUT();
 
-			// Act
-			await sut.LoadAsync(default);
+            // Act
+            await sut.LoadAsync(default);
 
-			// Assert
-			_storageMock.Verify(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()), Times.Once());
-		}
+            // Assert
+            _storageMock.Verify(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()), Times.Once());
+        }
 
-		[Fact(DisplayName = "[UNIT][CSV/LOAD-002] - Load Characters")]
-		public async Task CharacterService_LoadAsync_LoadCharacter()
-		{
-			// Arrange
-			var sut = CreateSUT();
-			var faker = new Faker();
-			var itemTypes = new List<ItemType> { new ItemTypeFaker().Generate(), new ItemTypeFaker().Selected().Generate() };
-			var characters = new CharacterFaker().Filter(itemTypes).Generate(3);
+        [Fact(DisplayName = "[UNIT][CSV/LOAD-002] - Load Characters")]
+        public async Task CharacterService_LoadAsync_LoadCharacter()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var itemTypes = new List<ItemType> { new ItemTypeFaker().Generate(), new ItemTypeFaker().Selected().Generate() };
+            var characters = new CharacterFaker().Filter(itemTypes).Generate(3);
 
-			_storageMock.Setup(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()))
-				.ReturnsAsync(JsonConvert.SerializeObject(characters.AsResponse()));
+            _storageMock.Setup(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(JsonConvert.SerializeObject(characters.AsResponse()));
 
-			_senderMock.Setup(s => s.SendAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(It.IsAny<GetItemTypesQuery>(), It.IsAny<CancellationToken>()))
-				.ReturnsAsync(itemTypes.AsResponse());
+            _senderMock.Setup(s => s.SendAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(It.IsAny<GetItemTypesQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(itemTypes.AsResponse());
 
-			// Act
-			await sut.LoadAsync(default);
+            // Act
+            await sut.LoadAsync(default);
 
-			// Assert
-			Assert.Collection(sut.Characters, characters.AsInspectors().ToArray());
-		}
+            // Assert
+            Assert.Collection(sut.Characters, characters.AsInspectors().ToArray());
+        }
 
-		[Fact(DisplayName = "[UNIT][CSV/LOAD-003] - Raise Loaded Event")]
-		public async Task CharacterService_LoadAsync_RaiseLoadedEvent()
-		{
-			// Arrange
-			var sut = CreateSUT();
+        [Fact(DisplayName = "[UNIT][CSV/LOAD-003] - Raise Loaded Event")]
+        public async Task CharacterService_LoadAsync_RaiseLoadedEvent()
+        {
+            // Arrange
+            var sut = CreateSUT();
 
-			_storageMock.Setup(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()))
-				.ReturnsAsync(JsonConvert.SerializeObject(new CharacterFaker().GenerateBetween(1, 5).AsResponse()));
+            _storageMock.Setup(s => s.GetItemAsStringAsync("characters", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(JsonConvert.SerializeObject(new CharacterFaker().GenerateBetween(1, 5).AsResponse()));
 
-			// Act
-			// Assert
-			await Assert.RaisesAsync<EventArgs>((handler) => sut.Loaded += handler, (handler) => sut.Loaded -= handler, async () => await sut.LoadAsync(default));
-		}
+            // Act
+            // Assert
+            await Assert.RaisesAsync<EventArgs>((handler) => sut.Loaded += handler, (handler) => sut.Loaded -= handler, async () => await sut.LoadAsync(default));
+        }
 
-		[Fact(DisplayName = "[UNIT][CSV/LOAD-016] - Querying Item Types")]
-		public async Task CharacterService_LoadAsync_QueryingItemTypes()
-		{
-			// Arrange
-			var sut = CreateSUT();
+        [Fact(DisplayName = "[UNIT][CSV/LOAD-016] - Querying Item Types")]
+        public async Task CharacterService_LoadAsync_QueryingItemTypes()
+        {
+            // Arrange
+            var sut = CreateSUT();
 
-			// Act
-			await sut.LoadAsync(default);
+            // Act
+            await sut.LoadAsync(default);
 
-			// Assert
-			_senderMock.Verify(s => s.SendAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(It.IsAny<GetItemTypesQuery>(), It.IsAny<CancellationToken>()));
-		}
-	}
+            // Assert
+            _senderMock.Verify(s => s.SendAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(It.IsAny<GetItemTypesQuery>(), It.IsAny<CancellationToken>()));
+        }
+    }
 }
--- src/RuneGlossary.Resurrected.WASM/Services/CharacterService.cs
+++ src/RuneGlossary.Resurrected.WASM/Services/CharacterService.cs
@@ -5,179 +5,180 @@ using RuneGlossary.Resurrected.Api;
 using RuneGlossary.Resurrected.WASM.Defaults;
 using RuneGlossary.Resurrected.WASM.Models;
 using STrain;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("RuneGlossary.Resurrected.Test.Unit")]
 namespace RuneGlossary.Resurrected.WASM.Services
 {
-	public interface ICharacterService
-	{
-		IEnumerable<Character> Characters { get; }
-		int Index { get; }
-		Character? Current { get; }
+    public interface ICharacterService
+    {
+        IEnumerable<Character> Characters { get; }
+        int Index { get; }
+        Character? Current { get; }
 
-		event EventHandler<EventArgs> Loaded;
-		event EventHandler<EventArgs> Changed;
+        event EventHandler<EventArgs> Loaded;
+        event EventHandler<EventArgs> Changed;
 
-		Task AddAsync(Character character, CancellationToken cancellationToken);
-		Task DeleteAsync(Character character, CancellationToken cancellationToken);
-		Task LoadAsync(CancellationToken cancellationToken);
-		Task SaveAsync(CancellationToken cancellationToken);
-		void Next();
-		void Previous();
-	}
+        Task AddAsync(Character character, CancellationToken cancellationToken);
+        Task DeleteAsync(Character character, CancellationToken cancellationToken);
+        Task LoadAsync(CancellationToken cancellationToken);
+        Task SaveAsync(CancellationToken cancellationToken);
+        void Next();
+        void Previous();
+    }
 
-	public class CharacterService(ILocalStorageService storage, IRequestSender sender) : ICharacterService
-	{
-		private const string KEY = "characters";
+    public class CharacterService(ILocalStorageService storage, IRequestSender sender) : ICharacterService
+    {
+        private const string KEY = "characters";
 
-		private IEnumerable<GetItemTypesQuery.Result> _itemTypes = [];
+        private IEnumerable<GetItemTypesQuery.Result> _itemTypes = [];
 
-		private readonly ILocalStorageService _storage = storage;
-		private readonly IRequestSender _sender = sender;
+        private readonly ILocalStorageService _storage = storage;
+        private readonly IRequestSender _sender = sender;
 
-		private ICollection<Character> _characters = [];
-		public IEnumerable<Character> Characters => _characters;
+        private ICollection<Character> _characters = [];
+        public IEnumerable<Character> Characters => _characters;
 
-		private int _index = 0;
-		public int Index => _index;
-		public Character? Current => _characters.ElementAtOrDefault(_index);
+        public int Index { get; private set; } = 0;
+        public Character? Current => _characters.ElementAtOrDefault(Index);
 
-		public event EventHandler<EventArgs>? Loaded;
-		public event EventHandler<EventArgs>? Changed;
+        public event EventHandler<EventArgs>? Loaded;
+        public event EventHandler<EventArgs>? Changed;
 
-		public async Task LoadAsync(CancellationToken cancellationToken)
-		{
-			_itemTypes = await _sender.GetAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(new GetItemTypesQuery(), cancellationToken) ?? [];
+        public async Task LoadAsync(CancellationToken cancellationToken)
+        {
+            _itemTypes = await _sender.GetAsync<GetItemTypesQuery, IEnumerable<GetItemTypesQuery.Result>>(new GetItemTypesQuery(), cancellationToken) ?? [];
 
-			var raw = await _storage.GetItemAsStringAsync(KEY, cancellationToken);
-			if (!string.IsNullOrWhiteSpace(raw))
-			{
-				var data = JsonConvert.DeserializeObject<IEnumerable<Data>>(raw, new JsonSerializerSettings
-				{
-					Converters = [new StringEnumConverter()]
-				}) ?? [];
-				_characters = data.Select(d => new Character
-				{
-					Name = d.Name,
-					Class = d.Class,
-					Level = d.Level,
-					Filters = new FilterData
-					{
-						ItemTypes = _itemTypes!.Select(it => new ItemType { Id = it.Id, Class = it.Class, Name = it.Name, Selected = d.Filters.Contains(it.Id) }).ToList()
-					}
-				}).ToList();
-			}
+            var raw = await _storage.GetItemAsStringAsync(KEY, cancellationToken);
+            if (!string.IsNullOrWhiteSpace(raw))
+            {
+                var data = JsonConvert.DeserializeObject<IEnumerable<Data>>(raw, new JsonSerializerSettings
+                {
+                    Converters = [new StringEnumConverter()]
+                }) ?? [];
+                _characters = [.. data.Select(d => new Character
+                {
+                    Name = d.Name,
+                    Class = d.Class,
+                    Level = d.Level,
+                    Filters = new FilterData
+                    {
+                        ItemTypes = _itemTypes!.Select(it => new ItemType { Id = it.Id, Class = it.Class, Name = it.Name, Selected = d.Filters.Contains(it.Id) }).ToList()
+                    }
+                })];
+            }
 
-			foreach (var character in _characters)
-			{
-				character.PropertyChanged += (_, __) => Changed?.Invoke(this, EventArgs.Empty);
+            foreach (var character in _characters)
+            {
+                character.PropertyChanged += (_, __) => Changed?.Invoke(this, EventArgs.Empty);
 
-				foreach (var filter in character.Filters.ItemTypes)
-				{
-					filter.PropertyChanged += (_, __) => Changed?.Invoke(this, EventArgs.Empty);
-				}
-			}
+                foreach (var filter in character.Filters.ItemTypes)
+                {
+                    filter.PropertyChanged += (_, __) => Changed?.Invoke(this, EventArgs.Empty);
+                }
+            }
 
-			Loaded?.Invoke(this, EventArgs.Empty);
-		}
+            Loaded?.Invoke(this, EventArgs.Empty);
+        }
 
-		public void Next()
-		{
-			if (_index == _characters.Count() - 1) return;
-			_index++;
+        public void Next()
+        {
+            if (Index == _characters.Count - 1) return;
+            Index++;
 
-			Changed?.Invoke(this, EventArgs.Empty);
-		}
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
 
-		public void Previous()
-		{
-			if (_index == 0) return;
-			_index--;
+        public void Previous()
+        {
+            if (Index == 0) return;
+            Index--;
 
-			Changed?.Invoke(this, EventArgs.Empty);
-		}
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
 
-		public async Task AddAsync(Character character, CancellationToken cancellationToken)
-		{
-			character.Filters = new FilterData
-			{
-				ItemTypes = _itemTypes.GetDefaultFilters(character.Class)
-			};
-			_characters.Add(character);
-			NavigateTo(_characters.Count - 1);
+        public async Task AddAsync(Character character, CancellationToken cancellationToken)
+        {
+            character.Filters = new FilterData
+            {
+                ItemTypes = _itemTypes.GetDefaultFilters(character.Class)
+            };
+            _characters.Add(character);
+            NavigateTo(_characters.Count - 1);
 
-			await SaveAsync(cancellationToken);
-		}
+            await SaveAsync(cancellationToken);
+        }
 
-		private void NavigateTo(int index)
-		{
-			_index = index;
-			Changed?.Invoke(this, EventArgs.Empty);
-		}
+        private void NavigateTo(int index)
+        {
+            Index = index;
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
 
-		public async Task DeleteAsync(Character character, CancellationToken cancellationToken)
-		{
-			_characters.Remove(character);
-			NavigateTo(_index == _characters.Count ? --_index : _index);
-			await SaveAsync(cancellationToken);
-		}
+        public async Task DeleteAsync(Character character, CancellationToken cancellationToken)
+        {
+            _characters.Remove(character);
+            NavigateTo(Index == _characters.Count ? --Index : Index);
+            await SaveAsync(cancellationToken);
+        }
 
-		public async Task SaveAsync(CancellationToken cancellationToken)
-		{
-			await _storage.SetItemAsync(KEY, _characters.Select(c => new Data
-			{
-				Name = c.Name,
-				Level = c.Level,
-				Class = c.Class,
-				Filters = c.Filters.ItemTypes.Where(f => f.Selected).Select(f => f.Id).ToList()
-			}).ToList(), cancellationToken);
-		}
-	}
+        public async Task SaveAsync(CancellationToken cancellationToken)
+        {
+            await _storage.SetItemAsync(KEY, _characters.Select(c => new Data
+            {
+                Name = c.Name,
+                Level = c.Level,
+                Class = c.Class,
+                Filters = c.Filters.ItemTypes.Where(f => f.Selected).Select(f => f.Id).ToList()
+            }).ToList(), cancellationToken);
+        }
+    }
 
-	file class Data()
-	{
-		public required string Name { get; init; }
-		public required Class Class { get; init; }
-		public required int Level { get; set; }
-		public required IEnumerable<int> Filters { get; set; }
-	}
+    file sealed record Data
+    {
+        public required string Name { get; init; }
+        public required Class Class { get; init; }
+        public required int Level { get; set; }
+        public required IEnumerable<int> Filters { get; set; }
+    }
 
-	file static class CharacterServiceExtensions
-	{
-		public static IEnumerable<ItemType> GetDefaultFilters(this IEnumerable<GetItemTypesQuery.Result> filters, Class @class)
-		{
-			var result = filters.Select(it => new ItemType { Id = it.Id, Name = it.Name, Class = it.Class, Selected = false }).ToList();
-			var selected = filters.Select(it => it.Id).ToList();
-			switch (@class)
-			{
-				case Class.Amazon:
-					selected = ClassDefaults.Filters.Amazon.ToList();
-					break;
-				case Class.Assassin:
-					selected = ClassDefaults.Filters.Assassin.ToList();
-					break;
-				case Class.Barbarian:
-					selected = ClassDefaults.Filters.Barbarian.ToList();
-					break;
-				case Class.Druid:
-					selected = ClassDefaults.Filters.Druid.ToList();
-					break;
-				case Class.Necromancer:
-					selected = ClassDefaults.Filters.Necromancer.ToList();
-					break;
-				case Class.Paladin:
-					selected = ClassDefaults.Filters.Paladin.ToList();
-					break;
-				case Class.Sorceress:
-					selected = ClassDefaults.Filters.Sorceress.ToList();
-					break;
-			}
+    file static class CharacterServiceExtensions
+    {
+        public static IEnumerable<ItemType> GetDefaultFilters(this IEnumerable<GetItemTypesQuery.Result> filters, Class @class)
+        {
+            var result = filters.Select(it => new ItemType { Id = it.Id, Name = it.Name, Class = it.Class, Selected = false }).ToList();
+            var selected = filters.Select(it => it.Id).ToList();
+            switch (@class)
+            {
+                case Class.Amazon:
+                    selected = ClassDefaults.Filters.Amazon.ToList();
+                    break;
+                case Class.Assassin:
+                    selected = ClassDefaults.Filters.Assassin.ToList();
+                    break;
+                case Class.Barbarian:
+                    selected = ClassDefaults.Filters.Barbarian.ToList();
+                    break;
+                case Class.Druid:
+                    selected = ClassDefaults.Filters.Druid.ToList();
+                    break;
+                case Class.Necromancer:
+                    selected = ClassDefaults.Filters.Necromancer.ToList();
+                    break;
+                case Class.Paladin:
+                    selected = ClassDefaults.Filters.Paladin.ToList();
+                    break;
+                case Class.Sorceress:
+                    selected = ClassDefaults.Filters.Sorceress.ToList();
+                    break;
+            }
 
-			foreach (var id in selected)
-			{
-				result.SingleOrDefault(it => it.Id == id)!.Selected = true;
-			}
+            foreach (var id in selected)
+            {
+                result.SingleOrDefault(it => it.Id == id)!.Selected = true;
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
