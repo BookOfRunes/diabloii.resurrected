012a0ce89ba2a04178538499e07df83215d7195d
Mihucza Bence (uids6410) <Bence.Mihucza-EXT@continental-corporation.com>
2025-03-11 09:00:50 +0100

    test(e2e): Implemented e2e test for filtering item types

--- src/BookOfRunes.DiabloII.Resurrected.Test.E2E/PageObjects/IndexPageObject.cs
+++ src/BookOfRunes.DiabloII.Resurrected.Test.E2E/PageObjects/IndexPageObject.cs
@@ -4,152 +4,152 @@ using Microsoft.Playwright;
 
 namespace BookOfRunes.DiabloII.Resurrected.Test.E2E.PageObjects
 {
-	public class IndexPageObject
-	{
-		private bool _firstCharacter = true;
+    public class IndexPageObject
+    {
+        private bool _firstCharacter = true;
 
-		private readonly string[] _classes = ["Amazon", "Assasin", "Necromancer", "Barbarian", "Paladin", "Sorceress", "Druid"];
+        private readonly string[] _classes = ["Amazon", "Assasin", "Necromancer", "Barbarian", "Paladin", "Sorceress", "Druid"];
 
-		private readonly IPage _page;
-		private readonly TestContext _context;
+        private readonly IPage _page;
+        private readonly TestContext _context;
 
-		public string? Name => _page.GetByTestId("spnName").TextContentAsync().GetAwaiter().GetResult();
-		public int? Level => int.Parse(_page.GetByTestId("inpLevel").InputValueAsync().GetAwaiter().GetResult());
+        public string? Name => _page.GetByTestId("spnName").TextContentAsync().GetAwaiter().GetResult();
+        public int? Level => int.Parse(_page.GetByTestId("inpLevel").InputValueAsync().GetAwaiter().GetResult());
 
-		public int SocketFrom
-		{
-			get { return int.Parse(_page.GetByTestId("inpSocketFrom").InputValueAsync().GetAwaiter().GetResult()); }
-			set { _page.GetByTestId("inpSocketFrom").FillAsync(value.ToString()).GetAwaiter().GetResult(); }
-		}
-		public int SocketTo
-		{
-			get { return int.Parse(_page.GetByTestId("inpSocketTo").InputValueAsync().GetAwaiter().GetResult()); }
-			set { _page.GetByTestId("inpSocketTo").FillAsync(value.ToString()).GetAwaiter().GetResult(); }
-		}
+        public int SocketFrom
+        {
+            get => int.Parse(_page.GetByTestId("inpSocketFrom").InputValueAsync().GetAwaiter().GetResult()); set => _page.GetByTestId("inpSocketFrom").FillAsync(value.ToString()).GetAwaiter().GetResult();
+        }
+        public int SocketTo
+        {
+            get => int.Parse(_page.GetByTestId("inpSocketTo").InputValueAsync().GetAwaiter().GetResult()); set => _page.GetByTestId("inpSocketTo").FillAsync(value.ToString()).GetAwaiter().GetResult();
+        }
 
-		public int LevelFilter
-		{
-			get { return int.Parse(_page.GetByTestId("inpLevelFilter").InputValueAsync().GetAwaiter().GetResult()); }
-			set { _page.GetByTestId("inpLevelFilter").FillAsync(value.ToString()).GetAwaiter().GetResult(); }
-		}
+        public int LevelFilter
+        {
+            get => int.Parse(_page.GetByTestId("inpLevelFilter").InputValueAsync().GetAwaiter().GetResult()); set => _page.GetByTestId("inpLevelFilter").FillAsync(value.ToString()).GetAwaiter().GetResult();
+        }
 
-		public IEnumerable<string> RuneWords
-		{
-			get { return _page.QuerySelectorAllAsync("[data-testid=spnRuneWord]").GetAwaiter().GetResult().Select(rw => rw.TextContentAsync().GetAwaiter().GetResult()!); }
-		}
+        public IEnumerable<string> RuneWords => _page.QuerySelectorAllAsync("[data-testid=spnRuneWord]").GetAwaiter().GetResult().Select(rw => rw.TextContentAsync().GetAwaiter().GetResult()!).ToList();
 
-		public IndexPageObject(IPage page, TestContext context)
-		{
-			_page = page;
-			_context = context;
-		}
+        public IndexPageObject(IPage page, TestContext context)
+        {
+            _page = page;
+            _context = context;
+        }
 
-		public async Task OpenAsync()
-		{
-			await _page.GotoAsync("/", new PageGotoOptions());
-		}
+        public async Task OpenAsync()
+        {
+            await _page.GotoAsync("/", new PageGotoOptions());
+        }
 
-		public async Task OpenCharacterSelectorAsync()
-		{
-			await _page.GetByTestId("btnAdd").ClickAsync();
-			await _page.GetByTestId("dlgAddCharacter").WaitForAsync();
-		}
+        public async Task OpenCharacterSelectorAsync()
+        {
+            await _page.GetByTestId("btnAdd").ClickAsync();
+            await _page.GetByTestId("dlgAddCharacter").WaitForAsync();
+        }
 
-		public async Task AddCharacterAsync(string name, int level)
-		{
-			if (!_firstCharacter) await OpenCharacterSelectorAsync();
+        public async Task AddCharacterAsync(string name, int level)
+        {
+            if (!_firstCharacter) await OpenCharacterSelectorAsync();
 
-			var @class = new Faker().PickRandom(_classes);
-			await _page.GetByTestId($"cls{@class}").ClickAsync();
+            var @class = new Faker().PickRandom(_classes);
+            await _page.GetByTestId($"cls{@class}").ClickAsync();
 
-			await _page.GetByTestId("inpName").FillAsync(name);
-			await _page.GetByTestId("inpAddCharacterLevel").FillAsync(level.ToString());
-			await _page.GetByTestId("btnSaveCharacter").ClickAsync();
+            await _page.GetByTestId("inpName").FillAsync(name);
+            await _page.GetByTestId("inpAddCharacterLevel").FillAsync(level.ToString());
+            await _page.GetByTestId("btnSaveCharacter").ClickAsync();
 
+            await _page.GetByTestId("dlgAddCharacter").WaitForAsync(new LocatorWaitForOptions
+            {
+                State = WaitForSelectorState.Detached
+            });
 
-			await _page.GetByTestId("dlgAddCharacter").WaitForAsync(new LocatorWaitForOptions
-			{
-				State = WaitForSelectorState.Detached
-			});
+            _firstCharacter = false;
+        }
 
-			_firstCharacter = false;
-		}
+        public async Task DeleteCharacterAsync(string name)
+        {
+            await _page.ReloadAsync();
 
-		public async Task DeleteCharacterAsync(string name)
-		{
-			await _page.ReloadAsync();
+            await _page.GetByTestId("btnDelete").ClickAsync();
 
-			await _page.GetByTestId("btnDelete").ClickAsync();
+            await _page.ReloadAsync();
+        }
 
-			await _page.ReloadAsync();
-		}
+        public async Task LevelUpAsync()
+        {
+            await _page.GetByTestId("btnIncreaseLevel").ClickAsync();
+            await _page.GetByTestId("btnSave").ClickAsync();
 
-		public async Task LevelUpAsync()
-		{
-			await _page.GetByTestId("btnIncreaseLevel").ClickAsync();
-			await _page.GetByTestId("btnSave").ClickAsync();
+            await _page.ReloadAsync();
+        }
 
-			await _page.ReloadAsync();
-		}
+        public async Task SelectAsync(string name)
+        {
+            await _page.ReloadAsync();
 
-		public async Task SelectAsync(string name)
-		{
-			await _page.ReloadAsync();
+            while (Name != name)
+            {
+                await SelectNextAsync();
+            }
+        }
 
-			while (Name != name)
-			{
-				await SelectNextAsync();
-			}
-		}
+        public async Task FilterAsync()
+        {
+            await _page.RunAndWaitForRequestFinishedAsync(async () => await _page.GetByTestId("btnFilter").ClickAsync(), new PageRunAndWaitForRequestFinishedOptions
+            {
+                Predicate = request => request.Headers["run-id"] == _context.RunId.ToString()
+            });
+            await Task.Delay(100);
+        }
 
-		public async Task FilterAsync()
-		{
-			await _page.RunAndWaitForRequestFinishedAsync(async () => await _page.GetByTestId("btnFilter").ClickAsync(), new PageRunAndWaitForRequestFinishedOptions
-			{
-				Predicate = request => request.Headers["run-id"] == _context.RunId.ToString()
-			});
-		}
+        public async Task SelectNextAsync()
+        {
+            await _page.GetByTestId("btnNext").ClickAsync();
+        }
 
-		public async Task SelectNextAsync()
-		{
-			await _page.GetByTestId("btnNext").ClickAsync();
-		}
+        public async Task SelectPreviousAsync()
+        {
+            await _page.GetByTestId("btnPrevious").ClickAsync();
+        }
 
-		public async Task SelectPreviousAsync()
-		{
-			await _page.GetByTestId("btnPrevious").ClickAsync();
-		}
+        public async Task SelectRuneAsync(string rune)
+        {
+            await _page.GetByTestId($"chb{rune}").ClickAsync();
+        }
 
-		public async Task SelectRuneAsync(string rune)
-		{
-			await _page.GetByTestId($"chb{rune}").ClickAsync();
-		}
+        public async Task SaveAsync()
+        {
+            await _page.GetByTestId("btnSave").ClickAsync();
+        }
 
-		public async Task SaveAsync()
-		{
-			await _page.GetByTestId("btnSave").ClickAsync();
-		}
+        public async Task SelectItemTypeAsync(string itemType)
+        {
+            await _page.GetByTestId($"chb{itemType}").ClickAsync();
+        }
 
-		public async Task SelectItemTypeAsync(string itemType)
-		{
-			await _page.GetByTestId($"chb{itemType}").ClickAsync();
-		}
+        public async Task ClearItemTypesFilterAsync()
+        {
+            await _page.GetByTestId("btnArmors").ClickAsync();
+            await _page.GetByTestId("btnWeapons").ClickAsync();
+        }
 
-		public async Task<bool> IsShownCharaterCreatorAsync()
-		{
-			return (await _page.WaitForSelectorAsync("[data-testid=dlgAddCharacter]")) is not null;
-		}
+        public async Task<bool> IsShownCharaterCreatorAsync()
+        {
+            return (await _page.WaitForSelectorAsync("[data-testid=dlgAddCharacter]")) is not null;
+        }
 
-		public async Task<bool> IsRuneSelectedAsync(string rune)
-		{
-			await _page.ReloadAsync();
-			return await _page.GetByTestId($"chb{rune}").IsCheckedAsync();
-		}
+        public async Task<bool> IsRuneSelectedAsync(string rune)
+        {
+            await _page.ReloadAsync();
+            return await _page.GetByTestId($"chb{rune}").IsCheckedAsync();
+        }
 
-		public async Task<bool> IsItemTypeSelectedAsync(string itemType)
-		{
-			await _page.ReloadAsync();
-			return await _page.GetByTestId($"chb{itemType}").IsCheckedAsync();
-		}
-	}
+        public async Task<bool> IsItemTypeSelectedAsync(string itemType)
+        {
+            await _page.ReloadAsync();
+            return await _page.GetByTestId($"chb{itemType}").IsCheckedAsync();
+        }
+    }
 }
--- src/BookOfRunes.DiabloII.Resurrected.Test.E2E/StepDefinitions/RuneWordStepDefinitions.cs
+++ src/BookOfRunes.DiabloII.Resurrected.Test.E2E/StepDefinitions/RuneWordStepDefinitions.cs
@@ -2,88 +2,86 @@
 
 namespace BookOfRunes.DiabloII.Resurrected.Test.E2E.StepDefinitions
 {
-	[Binding]
-	public class RuneWordStepDefinitions
-	{
-		private readonly IndexPageObject _pageObject;
+    [Binding]
+    public class RuneWordStepDefinitions
+    {
+        private readonly IndexPageObject _pageObject;
 
-		public RuneWordStepDefinitions(IndexPageObject pageObject)
-		{
-			_pageObject = pageObject;
-		}
+        public RuneWordStepDefinitions(IndexPageObject pageObject)
+        {
+            _pageObject = pageObject;
+        }
 
+        [Given("{string} item type filter is selected")]
+        public async Task SelectItemTypeAsync(string itemType)
+        {
+            await _pageObject.ClearItemTypesFilterAsync();
+            await _pageObject.SelectItemTypeAsync(itemType);
+        }
 
-		[Given("{string} item type filter is selected")]
-		public async Task SelectItemTypeAsync(string itemType)
-		{
-			await _pageObject.SelectItemTypeAsync(itemType);
-		}
+        [Given("Socket from is set to {int}")]
+        public void SetSocketFrom(int socketFrom)
+        {
+            _pageObject.SocketFrom = socketFrom;
+        }
 
-		[Given("Socket from is set to {int}")]
-		public void SetSocketFrom(int socketFrom)
-		{
-			_pageObject.SocketFrom = socketFrom;
-		}
+        [Given("Socket to is set to {int}")]
+        public void SetSocketTo(int socketTo)
+        {
+            _pageObject.SocketTo = socketTo;
+        }
 
-		[Given("Socket to is set to {int}")]
-		public void SetSocketTo(int socketTo)
-		{
-			_pageObject.SocketTo = socketTo;
-		}
+        [Given("Level filter is set to {int}")]
+        public void LevelFilterIsSetTo(int level)
+        {
+            _pageObject.LevelFilter = level;
+        }
 
-		[Given("Level filter is set to {int}")]
-		public void LevelFilterIsSetTo(int level)
-		{
-			_pageObject.LevelFilter = level;
-		}
+        [When("Filtering rune words")]
+        public async Task FilterAsync()
+        {
+            await _pageObject.FilterAsync();
+        }
 
+        [Then("{string} item type filter should be saved")]
+        public async Task ItemTypeShouldBeSelectedAsync(string itemType)
+        {
+            Assert.True(await _pageObject.IsItemTypeSelectedAsync(itemType));
+        }
 
-		[When("Filtering rune words")]
-		public async Task FilterAsync()
-		{
-			await _pageObject.FilterAsync();
-		}
+        [Then("Socket from should be {int}")]
+        public void SocketFromShouldBe(int socketFrom)
+        {
+            Assert.Equal(socketFrom, _pageObject.SocketFrom);
+        }
 
-		[Then("{string} item type filter should be saved")]
-		public async Task ItemTypeShouldBeSelectedAsync(string itemType)
-		{
-			Assert.True(await _pageObject.IsItemTypeSelectedAsync(itemType));
-		}
+        [Then("Socket to should be {int}")]
+        public void SocketToShouldBe(int socketTo)
+        {
+            Assert.Equal(socketTo, _pageObject.SocketTo);
+        }
 
-		[Then("Socket from should be {int}")]
-		public void SocketFromShouldBe(int socketFrom)
-		{
-			Assert.Equal(socketFrom, _pageObject.SocketFrom);
-		}
+        [Then("Level filter should be {int}")]
+        public void LevelFilterShouldBe(int level)
+        {
+            Assert.Equal(level, _pageObject.LevelFilter);
+        }
 
-		[Then("Socket to should be {int}")]
-		public void SocketToShouldBe(int socketTo)
-		{
-			Assert.Equal(socketTo, _pageObject.SocketTo);
-		}
+        [Then("Following rune words are shown:")]
+        public void ShowRuneWords(DataTable table)
+        {
+            Assert.Collection(_pageObject.RuneWords, [.. table.AsInspectors()]);
+        }
+    }
 
-		[Then("Level filter should be {int}")]
-		public void LevelFilterShouldBe(int level)
-		{
-			Assert.Equal(level, _pageObject.LevelFilter);
-		}
-
-
-		[Then("Following rune words are shown:")]
-		public void ShowRuneWords(DataTable table)
-		{
-			Assert.Collection(_pageObject.RuneWords, [.. table.AsInspectors()]);
-		}
-	}
-
-	file static class RuneWordStepDefinitionsExtensions
-	{
-		public static IEnumerable<Action<string>> AsInspectors(this DataTable table)
-		{
-			foreach (var row in table.Rows)
-			{
-				yield return rw => Assert.Equal(row["name"], rw);
-			}
-		}
-	}
+    file static class RuneWordStepDefinitionsExtensions
+    {
+        public static IEnumerable<Action<string>> AsInspectors(this DataTable table)
+        {
+            foreach (var row in table.Rows)
+            {
+                yield return rw => Assert.Equal(row["name"], rw);
+            }
+        }
+    }
 }
