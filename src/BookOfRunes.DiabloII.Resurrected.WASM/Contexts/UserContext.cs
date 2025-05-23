a3458501a4ca45a1fdf79aaebb7d89b502d34e49
Mihucza, Bence <mihben@gmail.com>
2025-05-23 17:35:31 +0200

    feat: Integrated microsoft SSO

---
+++ src/BookOfRunes.DiabloII.Resurrected.WASM/Contexts/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BookOfRunes.DiabloII.Resurrected.WASM.Contexts
+{
+	public class UserContext
+	{
+		private ClaimsPrincipal _user;
+
+		public string Name => _user.Claims.Single(c => c.Type == "given_name").Value;
+
+		public async Task InitializeAsync(Task<AuthenticationState> authenticationState)
+		{
+			_user = (await authenticationState).User;
+		}
+	}
+}
--- src/BookOfRunes.DiabloII.Resurrected.WASM/Program.cs
+++ src/BookOfRunes.DiabloII.Resurrected.WASM/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using BookOfRunes.DiabloII.Resurrected.WASM;
+using BookOfRunes.DiabloII.Resurrected.WASM.Contexts;
 using BookOfRunes.DiabloII.Resurrected.WASM.Controls.Dialog;
 using BookOfRunes.DiabloII.Resurrected.WASM.Controls.LoadingScreen;
 using BookOfRunes.DiabloII.Resurrected.WASM.Services;
@@ -14,9 +15,19 @@ builder.UseLightinject();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddOidcAuthentication(options =>
+{
+	builder.Configuration.Bind("Authentication", options.ProviderOptions);
+	options.ProviderOptions.ResponseType = "id_token";
+});
+
 builder.Services.AddSingleton<IRuneService, RuneService>();
 builder.Services.AddSingleton<ICharacterService, CharacterService>();
 builder.Services.AddSingleton<IRuneWordService, RuneWordService>();
+
+builder.Services.AddScoped<UserContext>();
 
 builder.Services.AddSingleton<Loading>();
 builder.Services.AddSingleton<ILoading>(provider => provider.GetRequiredService<Loading>());
